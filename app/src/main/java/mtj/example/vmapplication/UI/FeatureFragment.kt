6437b87489692f5cc3afc0eafc69b651ee66aace
Mrutyunjay <msenapati2@gmail.com>
2021-09-18 21:53:21 +0530

    FeatureFragment Added

--- app/src/main/java/mtj/example/vmapplication/UI/FeatureFragment.kt
+++ app/src/main/java/mtj/example/vmapplication/UI/FeatureFragment.kt
@@ -1,4 +1,33 @@
 package mtj.example.vmapplication.UI
 
-class FeatureFragment {
+import android.os.Bundle
+import android.view.View
+import mtj.example.vmapplication.Base.BaseFragment
+import mtj.example.vmapplication.R
+import mtj.example.vmapplication.UI.roomBookingModule.RoomFragment
+
+class FeatureFragment : BaseFragment(){
+
+
+    companion object{
+        const val TAG = "FeatureFragment"
+
+        fun newInstance(): FeatureFragment {
+            val args = Bundle()
+            val fragment = FeatureFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+    override fun provideLayoutId(): Int = R.layout.extralayout
+
+    override fun setupView(view: View) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupObservers() {
+        TODO("Not yet implemented")
+    }
+
+
 }
